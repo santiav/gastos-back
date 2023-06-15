17e8c966e324a0221cb91877aebc232af2895d4c
santiav <santiav@gmail.com>
2023-06-14 22:32:53 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use(express.json())
 // Usuarios
 app.get("/api/usuarios", async (req, res) => {
     try {
-        res.setHeader("Access-Control-Allow-Origin", origen)
+
         const [rows] = await pool.query("SELECT * FROM cuentas");
         console.log("rows /api/usuarios", rows)
         res.send(rows)
@@ -36,7 +36,7 @@ app.get("/api/importe-total/:usuario", async (req, res) => {
 
     try {
         let usuario = req.params.usuario
-        res.setHeader("Access-Control-Allow-Origin", origen)
+
         const [rows] = await pool.query("SELECT SUM(importe) FROM gastos WHERE usuario = ? AND moneda = 'pesos'", [usuario])
         console.log("/api/importe-total/:usuario", rows)
         res.send(rows)
@@ -50,7 +50,7 @@ app.get("/api/importe-total/:usuario", async (req, res) => {
 app.get("/api/aportes", async (req, res) => {
 
     try {
-        res.setHeader("Access-Control-Allow-Origin", origen)
+
         const [rows] = await pool.query("SELECT item,rubro,importe, moneda, fechaGasto,comentarios, usuario  FROM gastos WHERE aporte = 1")
         console.log("/api/aportes", rows)
         res.send(rows)
@@ -67,7 +67,7 @@ app.get("/api/importe-aportes", async (req, res) => {
     // usuario = "santi" // localStorage.getItem("usuario")
 
     try {
-        res.setHeader("Access-Control-Allow-Origin", origen)
+
         const sql = "SELECT SUM(importe) FROM gastos WHERE aporte = 1"
         const [rows] = await pool.query(sql)
         console.log("/api/importe-aportes", rows)
@@ -85,7 +85,7 @@ app.get("/api/importe-aportes", async (req, res) => {
 app.get("/api/importe-aportes-santi", async (req, res) => {
     // usuario = "santi" // localStorage.getItem("usuario")
     try {
-        res.setHeader("Access-Control-Allow-Origin", origen)
+
         const sql = "SELECT SUM(importe) FROM gastos WHERE aporte = 1 AND usuario = 'santi'"
         const [rows] = await pool.query(sql)
         res.send(rows)
@@ -102,7 +102,7 @@ app.get("/api/importe-aportes-santi", async (req, res) => {
 app.get("/api/importe-aportes-sil", async (req, res) => {
     // usuario = "santi" // localStorage.getItem("usuario")
     try {
-        res.setHeader("Access-Control-Allow-Origin", origen)
+
         const sql = "SELECT SUM(importe) FROM gastos WHERE aporte = 1 AND usuario = 'syl'"
         const [rows] = await pool.query(sql)
         res.send(rows)
@@ -119,20 +119,19 @@ app.get("/api/importe-aportes-sil", async (req, res) => {
 // -----------------
 
 // Agregar gasto 
-app.post("/api/agregar-gasto", (req, res) => {
+app.post("/api/agregar-gasto", async (req, res) => {
 
     let data = req.body
-    res.setHeader("Access-Control-Allow-Origin", origen)
-    pool.query("INSERT INTO gastos SET ?", data, (err, results) => {
-        if (err) throw err
-        console.info("datos agregados!" + results)
-    })
+
+    await pool.query("INSERT INTO gastos SET ?", [data])
+    console.info("datos agregados!" + results)
+    res.redirect("/");
 })
 
 // Todos los gastos del usuario
 app.get("/api/ver-gastos/:usuario", async (req, res) => {
     try {
-        res.setHeader("Access-Control-Allow-Origin", origen)
+
         let usuario = req.params.usuario
         const [rows] = await pool.query("SELECT * FROM gastos WHERE usuario = ?;SELECT * FROM tarjetas WHERE usuario = ?; ", [usuario, usuario])
         res.send(rows)
@@ -144,67 +143,62 @@ app.get("/api/ver-gastos/:usuario", async (req, res) => {
 });
 
 // Editar gasto diario
-app.put("/api/editar-gasto/:id", (req, res) => {
+app.put("/api/editar-gasto/:id", async (req, res) => {
 
 
     const id = req.params.id
     let data = req.body
-    res.setHeader("Access-Control-Allow-Origin", origen)
-    pool.query("UPDATE gastos SET ? where id = ?", [data, id], (err, results) => {
-        if (err) throw err
-        console.info("gasto actualizado!" + results)
-    })
+
+    await pool.query("UPDATE gastos SET ? where id = ?", [data, id])
+    console.info("gasto actualizado!" + results)
+    res.redirect("/");
 })
 
 // Borrar gasto
-app.delete("/api/borrar-gasto/:id", (req, res) => {
+app.delete("/api/borrar-gasto/:id", async (req, res) => {
 
 
     let id = req.params.id
-    res.setHeader("Access-Control-Allow-Origin", origen)
-    pool.query("DELETE FROM gastos WHERE id = ?", [id], (err, results) => {
-        if (err) throw err
-        console.info("datos eliminados!" + results)
-    })
+
+    await pool.query("DELETE FROM gastos WHERE id = ?", [id])
+    console.info("datos eliminados!" + results)
+    res.redirect("/");
 })
 
 // CRUD Tarjeta
 
 // Agregar resumen tarjeta (gasto)
-app.post("/api/agregar-tarjeta", (req, res) => {
+app.post("/api/agregar-tarjeta", async (req, res) => {
 
-  
+
     let data = req.body
-    res.setHeader("Access-Control-Allow-Origin", origen)
-    pool.query("INSERT INTO tarjetas SET ?", data, (err, results) => {
-        if (err) throw err
+
+    await pool.query("INSERT INTO tarjetas SET ?", data)
         console.info("datos agregados!")
-    })
+    res.redirect("/");
 })
 
 // Editar gasto tarjeta
-app.put("/api/tarjeta/editar-gasto/:id", (req, res) => {
+app.put("/api/tarjeta/editar-gasto/:id", async (req, res) => {
 
 
     const id = req.params.id
     let data = req.body
-    res.setHeader("Access-Control-Allow-Origin", origen)
-    pool.query("UPDATE tarjetas SET ? where id = ?", [data, id], (err, results) => {
-        if (err) throw err
+
+    await pool.query("UPDATE tarjetas SET ? where id = ?", [data, id])
         console.info("gasto actualizado!")
-    })
+    res.redirect("/");
 })
 
 
 // Borrar gasto
-app.delete("/api/tarjeta/borrar-gasto/:id", (req, res) => {
+app.delete("/api/tarjeta/borrar-gasto/:id", async (req, res) => {
 
     let id = req.params.id
-    res.setHeader("Access-Control-Allow-Origin", origen)
-    pool.query("DELETE FROM tarjetas WHERE id = ?", [id], (err, results) => {
-        if (err) throw err
+
+    await pool.query("DELETE FROM tarjetas WHERE id = ?", [id])
         console.info("datos eliminados!")
-    })
+    res.redirect("/");
 })
 
 // FILTROS
@@ -214,21 +208,21 @@ app.get("/api/ver-gastos/:usuario/mes-actual", async (req, res) => {
     try {
         let usuario = req.params.usuario ?? "todos"
         let sql;
-        res.setHeader("Access-Control-Allow-Origin", origen)
+
         if (usuario != "todos") {
             sql = "select id, item,rubro,importe,tipoPago, moneda, fechaGasto,comentarios FROM `gastos` WHERE usuario = ? AND MONTH(fechaGasto) = MONTH(now()) AND YEAR(fechaGasto) = YEAR(now()) "
         } else {
 
             sql = "select * FROM `gastos` WHERE aporte = 1 AND MONTH(fechaGasto) = MONTH(now()) AND YEAR(fechaGasto) = YEAR(now()) "
         }
-        
+
         const [rows] = await pool.query(sql, [usuario])
         res.send(rows)
 
     } catch (err) {
         return res.status(500).json({ message: "Something goes wrong" + err })
     }
-    
+
 });
 
 
