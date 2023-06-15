45caaf113ea891ae6c5db54b864db965d88d3b4e
santiav <santiav@gmail.com>
2023-06-15 00:52:09 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,9 +7,7 @@ const app = express()
 const port = 4000
 
 // Middlewares
-app.use(cors({
-    origin: origen,
-}));
+app.use(cors());
 app.use(express.json())
 
 // const db = require('../model/db')
